cc7d7b89744a85456c3cbdab337f295b966eb352
carlinhoshk <https://carlinhoshk@github.com>
2020-10-28 21:33:07 -0300

    adicionado py menor

--- teste.py
+++ teste.py
@@ -1,9 +1,7 @@
-import numpy as np
-
-# criar um numero entre 1 e 10 
-
-a = np.arange(10) # criar uma conta até 10 no np
+a = "ola mundo"
 print(a)
-b = np.arange(1,6) # contar até fim de b 
-print(b)
+
+
+    
+
 
